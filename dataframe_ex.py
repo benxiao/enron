47f805f3c9cf650b09a1df313b6f41f0d1b87b21
ben xiao <ben.xiao@me.com>
2017-01-29 11:41:30 +1100

    add more files for visualization

---
+++ dataframe_ex.py
@@ -0,0 +1,27 @@
+from pandas import DataFrame
+
+
+data = {
+    'a': [1, 2],
+    'b': [3, 4]
+}
+
+data2 = [[1,2,3,4], [4,5,6,7]]
+
+df = DataFrame(data, columns=['b', 'a']) # garantee the order of the columns
+print(df)
+
+df2 = DataFrame(data2,columns=['a', 'c', 'b', 'd'])
+print(df2)
+
+data3 = [('a', 'b'),(1,2),(3,4)]
+df3 = DataFrame(data3[1:], columns=data3[0])
+print(df3)
+
+
+data4 = [('a', 'b'), ('c', 'd')]
+df4 = DataFrame(data4)
+print(df4)
+print(df4.values)
+print(type(df4.values))
+print(df4.values.tolist())
--- topic_chain.py
+++ topic_chain.py
@@ -16,28 +16,76 @@ class DynamicTopic:
         self._time_slice = time_slice
         self._topic_n = topic_n
 
+    @property
+    def ts(self):
+        return self._time_slice
+
+    @property
+    def ti(self):
+        return self._topic_n
+
+    @property
+    def top_words(self):
+        return [x[0] for x in self.top_word_distribution(1000)]
+
+    def top_word_distribution(self, n):
+        d = self._dm.get_topic_keys(self.ts, self.ti)
+        return sorted(d.items(), key=lambda x: -x[-1])[:n]
+
+
     def next(self):
-        related_future_topics = self._dm.get_outgoing_connections(self._time_slice, self._topic_n)
-        return [DynamicTopic(self._dm, self._time_slice+1, i) for i in related_future_topics]
+        next_time_slice = self._time_slice + 1
+        if next_time_slice > len(self._dm._conn):
+            return []
+        related_future_topic_indexes = self._dm.get_outgoing_connections(self._time_slice, self._topic_n)
+        return [DynamicTopic(self._dm, next_time_slice, i) for i in related_future_topic_indexes]
+
+    def prev(self):
+        prev_time_slice = self._time_slice-1
+        if prev_time_slice < 0: return []
+        related_previous_topic_indexes = [i for i, t in enumerate(self._dm._conn[prev_time_slice]) if self._topic_n in t]
+        return [DynamicTopic(self._dm, prev_time_slice, i) for i in related_previous_topic_indexes]
 
     def __str__(self):
-        return str(self._dm.get_topic_keys(self._time_slice, self._topic_n))
+        return 'DT:'+str((self._time_slice, self._topic_n))
 
     __repr__ = __str__
 
+    @classmethod
+    def common_words(cls, dynamic_topics, n):
+        common = set(dynamic_topics[0].top_words)
+        for i in range(1, len(dynamic_topics)):
+            common = common.intersection(set(dynamic_topics[i].top_words))
+        return list(common)[:n]
 
 class TopicChain:
     """
-    json format [[{topic(k:p)}, {topic}, {topic} ...]... more time_slices... ]
+    json format [[{topic(k:p)}, {topic}, {topic} ...]... more time_slices... ] == list(list(dict)))
     while k is the keyword and p is the probability
     """
     def __init__(self, json_, **kwargs):
         self._data = json.load(open(json_))
         self._data = [[{k: float(y[k]) for k in y} for y in x] for x in self._data]
-        self._threshold = 0.3
-        self._max_outgoing = 3
+
+        self._threshold = kwargs.get('threshold') or 0.35
+        self._max_outgoing = kwargs.get('max_outgoing') or 1
+        self._max_incoming = kwargs.get('max_incoming') or 2
+        self._nkeys = kwargs.get('nkeys') or 20
         self._conn = self._generate_conns_from_data()
 
+    @property
+    def table(self):
+        # don't write to it # pretend it is read only
+        return self._conn
+
+    @property
+    def nkeys(self):
+        return self._nkeys
+
+    @nkeys.setter
+    def nkeys(self, new_value):
+        self._nkeys = new_value
+        self._conn = self._generate_conns_from_data()
 
     @property
     def threshold(self):
@@ -57,6 +105,15 @@ class TopicChain:
         self._max_outgoing = new_value
         self._conn = self._generate_conns_from_data()
 
+    @property
+    def max_incoming(self):
+        return self._max_incoming
+
+    @max_incoming.setter
+    def max_incoming(self, new_value):
+        self._max_incoming = new_value
+        self._conn = self._generate_conns_from_data()
+
     def get_topics_with_outgoing_connections(self, time_slice):
         return [i for i, x in enumerate(self._conn[time_slice]) if len(x)]
 
@@ -74,21 +131,34 @@ class TopicChain:
         data = self._data
         for i in range(len(data) - 1):
             lda_topic_1 = data[i]
-            lda_topic_2 = data[i + 1]
+            lda_topic_2 = data[i+1]
             lda_topic_1 = [sorted([(topic[k], k) for k in topic], key=lambda x: -x[0]) for topic in lda_topic_1]
             lda_topic_2 = [sorted([(topic[k], k) for k in topic], key=lambda x: -x[0]) for topic in lda_topic_2]
             l = len(lda_topic_1)
             d_matrix = [[None] * l for _ in range(l)]
             for i in range(l):
                 for j in range(l):
-                    d_matrix[i][j] = self.compute_hellinger(lda_topic_1[i], lda_topic_2[j])
+                    d_matrix[i][j] = self.compute_hellinger(lda_topic_1[i][:self._nkeys], lda_topic_2[j][:self._nkeys])
             slice_result = []
             for group in self.get_edges_between_two_time_slices(d_matrix,
-                                                                threshold=self.threshold,
-                                                                max_outgoing=self.max_outgoing):
-                slice_result.append([x[1] for x in group])
+                                                                self.threshold,
+                                                                self.max_outgoing,
+                                                                self.max_incoming):
+                
+                slice_result.append(group)
             result.append(slice_result)
         return result
+
+    def show_conns(self):
+        for i, slice in enumerate(self._conn):
+            line = '{}:'.format(i) + str(''.join(['{}:{}| '.format(i, t) for i, t in enumerate(slice)]))
+            print(line)
+            print(len(line) * '-')
+
+
+    # def get_conns(self):
+    #     return
+
 
     @staticmethod
     def compute_hellinger(dist01, dist02):
@@ -100,26 +170,29 @@ class TopicChain:
         return hellinger(vec01, vec02)
 
     @staticmethod
-    def get_edges_between_two_time_slices(matrix, threshold=0.3, max_outgoing=3):
+    def get_edges_between_two_time_slices(distance_table, threshold, max_outgoing, max_incoming):
         lst = []
-        l = len(matrix)
+        l = len(distance_table)
+        # find outgoing connections under the limit
         for i in range(l):
-            outgoings = list(filter(lambda x: x[-1] < threshold, ((i, j, matrix[i][j]) for j in range(l))))
-            outgoings = sorted(outgoings, key=lambda x: x[-1])[:max_outgoing]
+            outgoings = list(filter(lambda x: distance_table[i][x] < threshold, range(l)))
+            if len(outgoings) > max_outgoing:
+                outgoings = sorted(outgoings, key=lambda x: distance_table[i][x])[:max_outgoing]
             lst.append(outgoings)
+
+        # restrict the number of incoming connections from the 'merge'
+        for j in range(l):
+            incoming_conns = [i for i, p in enumerate(lst) if j in p]
+            if len(incoming_conns) > max_incoming:
+                conns_removed =sorted(incoming_conns, key=lambda x: distance_table[x][j])[max_incoming:]
+                for i in conns_removed:
+                    lst[i].remove(j)
         return lst
 
 if __name__ == '__main__':
-    dm = TopicChain('json_files/topic_keys.json')
-    print(dm.get_topics_with_outgoing_connections(0))
-    print(dm.get_outgoing_connections(0, 2))
-    print(dm.get_topic_keys(0, 14))
-    print(dm.get_topic_keys(1, 0))
-    print(dm._conn)
-    dm.threshold = 0.4
-    print("*" * 200)
-    print(dm._conn)
-    print(dm.get_dynamic_topic(0,0))
-    print(dm.get_dynamic_topic(0,0).next())
-    dm.max_outgoing = 1
-    print(dm._conn)
+    dm = TopicChain('topic_keys.json',
+                    threshold=0.35,
+                    max_incoming=2,
+                    max_outgoing=1
+                    )
+
